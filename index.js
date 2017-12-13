b1f0f193a9cde3ea1fa8d6ec182d56054e7d94db
Fraser <mrfrase3@gmail.com>
2017-12-13 09:17:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,25 @@
   const template = Handlebars.compile($('#painting-template').html());
   const base_url = window.location.origin + (window.location.pathname.replace(/[^\\\/]*$/, '') || '/');
   const apainter_url = 'https://aframe.io/a-painter/';
+  const $paintings = $paintings;
+  let $currow = null;
+  let c = 0;
   const load_painting = (name) => {
     $.get('./paintings/'+name+'/about.json').done(about=>{
+      if(c%3 == 0){
+        $currow = $('<div class="row"></div>');
+        $paintings.append(currow);
+      }
+      c++;
       let context = {
         name: about.name,
         authors: about.authors,
         created: moment(about.created).format('llll'),
         imageurl: `${base_url}paintings/${name}/Screenshot.png`,
-        url: `${apainter_url}?url=${about.url}`
+        url: `${apainter_url}?url=${about.url}`,
+        desc: about.desc || ""
       };
-      $('.row').append(template(context));
+      $currow.append(template(context));
     });
   };
 
